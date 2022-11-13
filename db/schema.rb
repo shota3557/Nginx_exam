41a0841db153e7e5b00ddc461c61dbae44009a26
shota kikuchi <shota3557@gmail.com>
2022-11-13 22:16:24 +0900

    herokuにデプロイするため

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2022_11_13_124711) do
+
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
